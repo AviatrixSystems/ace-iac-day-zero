34c84e91f0e626223226da8f2552e6cb1bb65068
hoodbu <38543715+hoodbu@users.noreply.github.com>
2021-07-21 15:21:43 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0.2"
   backend "remote" {
     hostname     = "app.terraform.io"
-    # organization = "<replace-with-your-Terraform-Cloud-workspace-and-uncomment>"
+    # organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>"
     workspaces {
       name = "ace-iac"
     }
