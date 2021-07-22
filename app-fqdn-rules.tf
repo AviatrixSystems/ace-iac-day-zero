280f4a5c189028d3b5047ad6170cb3493f8e580b
hoodbu <38543715+hoodbu@users.noreply.github.com>
2021-07-22 11:50:58 -0400

    Update app-fqdn-rules.tf

--- app-fqdn-rules.tf
+++ app-fqdn-rules.tf
@@ -4,12 +4,6 @@ locals {
     tcp = {
       "*.aviatrix.com" = "443"
       "aviatrix.com"   = "80"
-      "*.espn.com"     = "443"
-      "cnn.com"        = "443"
-      "nbc.com"        = "80"
-      "yahoo.com"      = "80"
-      "whitehouse.gov" = "80"
-      "mlb.com"        = "80"
     }
     udp = {
       "dns.google.com" = "53"
