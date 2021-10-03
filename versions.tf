bc8cd12b106133ab616c2b46149f558d739c734e
hoodbu <38543715+hoodbu@users.noreply.github.com>
2021-10-03 15:41:52 -0400

    Controller version 6.5

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aviatrix = {
       source  = "AviatrixSystems/aviatrix"
-      version = "~> 2.19.5"
+      version = "~> 2.20"
     }
     aws = {
       source  = "hashicorp/aws"
