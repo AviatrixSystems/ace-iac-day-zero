d7348b85d8226cb07f96550c27e00e71d59ac283
hoodbu <38543715+hoodbu@users.noreply.github.com>
2022-03-20 14:44:15 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.0.2"
+  required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
     # organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>"
