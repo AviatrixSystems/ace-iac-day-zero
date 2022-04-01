046f1b01ea13ac2bb9af1af449182d6358bdda00
hoodbu <38543715+hoodbu@users.noreply.github.com>
2022-04-01 12:20:24 -0400

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aviatrix = {
       source  = "AviatrixSystems/aviatrix"
-      version = "2.21.1"
+      version = "~> 2.21.2"
     }
     aws = {
       source  = "hashicorp/aws"
