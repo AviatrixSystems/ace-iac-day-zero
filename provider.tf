168aa75b7e9bf272df188fa46e02bc3be2982e64
hoodbu <38543715+hoodbu@users.noreply.github.com>
2022-04-01 15:23:23 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,8 +11,9 @@ provider "aws" {
 
 provider "azurerm" {
   features {}
-  subscription_id = var.azure_subscription_id
-  client_id       = var.azure_client_id
-  client_secret   = var.azure_client_secret
-  tenant_id       = var.azure_tenant_id
+  skip_provider_registration = "true"
+  subscription_id            = var.azure_subscription_id
+  client_id                  = var.azure_client_id
+  client_secret              = var.azure_client_secret
+  tenant_id                  = var.azure_tenant_id
 }
