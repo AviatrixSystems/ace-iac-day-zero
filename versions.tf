2a8dd986de88c4ed6e8f4c7149ec3e925b2731f4
ace-netops <105461767+ace-netops@users.noreply.github.com>
2022-06-14 16:56:24 -0400

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aviatrix = {
       source  = "AviatrixSystems/aviatrix"
-      version = "~> 2.21.2"
+      version = "~> 2.22.0"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
