880541ae899399f00e4dfe49e6ea109295a6c6c0
hoodbu <38543715+hoodbu@users.noreply.github.com>
2022-06-28 11:08:17 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "ace-netops"
+    # organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>"
     workspaces {
       name = "ace-iac-day-zero"
     }
