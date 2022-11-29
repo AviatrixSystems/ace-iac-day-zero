db9d59156144643d9440a83f833fbd28fe0869ee
Daniel Muanye <48682164+wdaniel24@users.noreply.github.com>
2022-11-29 17:06:18 +0000

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -2,7 +2,7 @@
 # Data source to get AMI details
 ##################################################################
 data "aws_ami" "ubuntu" {
-  provider    = aws.ohio
+  provider    = aws.london
   most_recent = true
   filter {
     name   = "name"
@@ -40,7 +40,7 @@ module "security_group_1" {
   ingress_rules       = ["http-80-tcp", "ssh-tcp", "all-icmp"]
   egress_rules        = ["all-all"]
   providers = {
-    aws = aws.ohio
+    aws = aws.london
   }
 }
 
@@ -57,7 +57,7 @@ module "aws_spoke_bastion" {
   associate_public_ip_address = true
   user_data_base64            = base64encode(local.bu1_bastion_user_data)
   providers = {
-    aws = aws.ohio
+    aws = aws.london
   }
 }
 
