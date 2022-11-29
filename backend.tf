e83c02ce7a0858460ac545691c082fd17c9c06ac
Daniel Muanye <48682164+wdaniel24@users.noreply.github.com>
2022-11-29 13:26:55 +0000

    Update backend.tf
    Updated organisation info

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    # organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>"
+    organization = "aviatrix-pro-netops"
     workspaces {
       name = "ace-iac-day-zero"
     }
