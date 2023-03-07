69f048d9fd9cd28ce517034ea345741e47d83d4d
karimjamali <karim.jamali@gmail.com>
2023-03-07 13:51:33 -0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aviatrix = {
       source  = "AviatrixSystems/aviatrix"
-      version = "~> 2.24.0"
+      version = "~> 3.0.1"
     }
     aws = {
       source  = "hashicorp/aws"
