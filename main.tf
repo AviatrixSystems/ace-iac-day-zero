346ed72074e81907d973e46019eed0192b709770
karimjamali <karim.jamali@gmail.com>
2023-03-07 16:38:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -84,3 +84,48 @@ resource "aviatrix_segmentation_network_domain_connection_policy" "BU1_BU2" {
   domain_name_2 = "BU2"
   depends_on    = [aviatrix_segmentation_network_domain.BU1, aviatrix_segmentation_network_domain.BU2]
 } 
+
+data "aviatrix_spoke_gateway" "egress" {
+  gw_name = var.azure_spoke2_name
+}
+
+resource "aviatrix_fqdn" "fqdn_filter" {
+  fqdn_tag     = "APP-RULES"
+  fqdn_mode    = "white"
+  fqdn_enabled = true
+  gw_filter_tag_list {
+    gw_name = data.aviatrix_spoke_gateway.egress.gw_name
+  }
+  manage_domain_names = false
+}
+
+resource "aviatrix_fqdn_tag_rule" "tcp" {
+  for_each      = local.egress_rules.tcp
+  fqdn_tag_name = aviatrix_fqdn.fqdn_filter.fqdn_tag
+  fqdn          = each.key
+  protocol      = "tcp"
+  port          = each.value
+  depends_on    = [aviatrix_fqdn.fqdn_filter]
+}
+
+resource "aviatrix_fqdn_tag_rule" "udp" {
+  for_each      = local.egress_rules.udp
+  fqdn_tag_name = aviatrix_fqdn.fqdn_filter.fqdn_tag
+  fqdn          = each.key
+  protocol      = "udp"
+  port          = each.value
+  depends_on    = [aviatrix_fqdn.fqdn_filter]
+}
+  
+  
+ locals {
+  egress_rules = {
+    tcp = {
+      "*.aviatrix.com" = "443"
+      "aviatrix.com"   = "80"
+    }
+    udp = {
+      "dns.google.com" = "53"
+    }
+  }
+}
