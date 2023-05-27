cfc44aa06ef57617f0e9a58960e9d22ba94f8e78
aldrintupas00 <134664410+aldrintupas00@users.noreply.github.com>
2023-05-27 16:29:38 +0800

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "aldrinAviatrix"
+    organization = "aldrinAviatrix2"
     workspaces {
-      name = "ace-iac-day-zero"
+      name = "ace-iac-day-two"
     }
   }
 }
