bbd4f9cda938b14cfa0924cb7c82b61da2c30229
aldrintupas00 <134664410+aldrintupas00@users.noreply.github.com>
2023-05-27 16:24:36 +0800

    Update app-fqdn-rules.tf

--- app-fqdn-rules.tf
+++ app-fqdn-rules.tf
@@ -4,7 +4,7 @@ locals {
      tcp = {
        "*.aviatrix.com" = "443"
        "aviatrix.com"   = "80"
-       "*.ubuntu.com" = "443"
+       "*.ubuntu.com" = "80"
      }
    udp = {
      "dns.google.com" = "53"
