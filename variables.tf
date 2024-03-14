4b701a054b6871def827b382e6fcaeb921de51aa
drewcornell0409 <163339473+drewcornell0409@users.noreply.github.com>
2024-03-14 12:21:07 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -16,7 +16,7 @@ variable "aws_account_name" {
 }
 
 variable "aws_transit_instance_size" {
-  default = "t2.micro"
+  default = "t3.micro"
 }
 
 variable "aws_transit1_region" {
@@ -32,7 +32,7 @@ variable "aws_transit1_cidr" {
 }
 
 variable "aws_spoke_instance_size" {
-  default = "t2.micro"
+  default = "t3.micro"
 }
 
 variable "aws_spoke1_region" {
