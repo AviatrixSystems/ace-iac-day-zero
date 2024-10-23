d6b9d61aeebb2e845f2416e31e2c21cdefdb9029
Sampataceadmindevnetsecopsadmin <sllohar123@yahoo.co.in>
2024-10-23 23:11:51 +0530

    Update main.tf-enhancement -bu1bu2
    BU1 -BU2

--- main.tf
+++ main.tf
@@ -82,8 +82,7 @@ resource "aviatrix_segmentation_network_domain" "BU2" {
 resource "aviatrix_segmentation_network_domain_connection_policy" "BU1_BU2" {
   domain_name_1 = aviatrix_segmentation_network_domain.BU1.domain_name
   domain_name_2 = aviatrix_segmentation_network_domain.BU2.domain_name
-
-} 
+}
 
 
 
