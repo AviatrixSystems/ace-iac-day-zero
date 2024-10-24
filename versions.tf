f88eaf8abc5236cc7677a43dbec21fab95e1dad0
yves1987 <118463123+yves1987@users.noreply.github.com>
2024-10-24 10:08:06 -0400

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,16 +2,16 @@ terraform {
   required_providers {
     aviatrix = {
       source  = "AviatrixSystems/aviatrix"
-      version = "~> 3.0.1"
+      version = "3.1.5"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.67.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0.0"
     }
   }
-  required_version = ">= 1.0"
+  required_version = "1.5.7"
 }
