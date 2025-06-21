0a5e0fc3dd63b0d6b5f610a87fd7c6bce1a58420
yuxingnan <monkey_yxn@yahoo.cn>
2025-06-21 19:49:40 +0800

    Connecting BU1 and BU2
    Connecting BU1 and BU2 via domain connection policy

--- main.tf
+++ main.tf
@@ -79,11 +79,11 @@ resource "aviatrix_segmentation_network_domain" "BU2" {
   ]
 }
   
-#resource "aviatrix_segmentation_network_domain_connection_policy" "BU1_BU2" {
-#  domain_name_1 = "BU1"
-#  domain_name_2 = "BU2"
-#  depends_on    = [aviatrix_segmentation_network_domain.BU1, aviatrix_segmentation_network_domain.BU2]
-#} 
+resource "aviatrix_segmentation_network_domain_connection_policy" "BU1_BU2" {
+  domain_name_1 = "BU1"
+  domain_name_2 = "BU2"
+  depends_on    = [aviatrix_segmentation_network_domain.BU1, aviatrix_segmentation_network_domain.BU2]
+} 
 
 
 
