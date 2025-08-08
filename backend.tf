a3f60c5de1ea5f2635ba11845bb01fd08fd281b8
DINESHKUMAR S <dineshku.s.ece.2021@snsct.org>
2025-08-08 11:44:17 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    # organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>"
+    # organization = "Dinesh_2201"
     workspaces {
       name = "ace-iac-day-zero"
     }
